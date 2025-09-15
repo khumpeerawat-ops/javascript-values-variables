5f77c37a058201a877b2c5cbfa82f17034c644b6
panuwat <khumpeerawat@gmail.com>
2025-09-15 20:33:25 +0700

    JavaScript Values Variables Assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,10 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 //Exercise #2: Variable Reassignment
 //Start coding here
+const restaurantName = "Chef's Table by Chef James";
+const totalPrice = 15000;
+const numberRestaurant = 1500;
